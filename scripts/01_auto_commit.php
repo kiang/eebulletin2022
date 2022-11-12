324bc051eb056c1f666c83baa72a2c000c797ab7
kiang <kiange@gmail.com>
2022-11-12 22:59:17 +0800

    reset size in the loop

--- scripts/01_auto_commit.php
+++ scripts/01_auto_commit.php
@@ -4,13 +4,14 @@ exec("/usr/bin/find {$path} -type f", $lines);
 
 $currentSize = 0;
 $limit = pow(1024, 2) * 500;
-foreach($lines AS $line) {
+foreach ($lines as $line) {
     $size = filesize($line);
     $currentSize += $size;
-    exec("/usr/bin/git add {$line}");
-    if($currentSize > $limit) {
+    exec("/usr/bin/git add '{$line}'");
+    if ($currentSize > $limit) {
         $now = date('Y-m-d H:i:s');
         exec("cd {$path} && /usr/bin/git commit --author 'auto commit <noreply@localhost>' -m 'auto update @ {$now}'");
         exec("cd {$path} && /usr/bin/git push origin master");
+        $currentSize = 0;
     }
 }
