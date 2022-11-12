67d87fe2df13cf14662dfda46b6f34b8917ed434
kiang <kiange@gmail.com>
2022-11-12 22:55:32 +0800

    script to push data

---
+++ scripts/01_auto_commit.php
@@ -0,0 +1,16 @@
+<?php
+$path = dirname(__DIR__);
+exec("/usr/bin/find {$path} -type f", $lines);
+
+$currentSize = 0;
+$limit = pow(1024, 2) * 500;
+foreach($lines AS $line) {
+    $size = filesize($line);
+    $currentSize += $size;
+    exec("/usr/bin/git add {$line}");
+    if($currentSize > $limit) {
+        $now = date('Y-m-d H:i:s');
+        exec("cd {$path} && /usr/bin/git commit --author 'auto commit <noreply@localhost>' -m 'auto update @ {$now}'");
+        exec("cd {$path} && /usr/bin/git push origin master");
+    }
+}
