a00fc8d9ff23e1dfab4bd9c4cfe4aff952772939
kiang <kiange@gmail.com>
2022-11-12 23:02:12 +0800

    add single file limit to skip

--- scripts/01_auto_commit.php
+++ scripts/01_auto_commit.php
@@ -4,8 +4,12 @@ exec("/usr/bin/find {$path} -type f", $lines);
 
 $currentSize = 0;
 $limit = pow(1024, 2) * 500;
+$skip = pow(1024, 2) * 100;
 foreach ($lines as $line) {
     $size = filesize($line);
+    if($size > $skip) {
+        continue; //github don't like file larger than 100M
+    }
     $currentSize += $size;
     exec("/usr/bin/git add '{$line}'");
     if ($currentSize > $limit) {
