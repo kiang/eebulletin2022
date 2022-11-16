83e7bd6eed542f0dd86ff9c1c5d732367a8de211
kiang <kiange@gmail.com>
2022-11-16 20:57:38 +0800

    create zip archive for large file

--- scripts/01_auto_commit.php
+++ scripts/01_auto_commit.php
@@ -5,13 +5,33 @@ exec("/usr/bin/find {$path} -type f", $lines);
 $currentSize = 0;
 $limit = pow(1024, 2) * 500;
 $skip = pow(1024, 2) * 100;
+$zip = new ZipArchive();
+
 foreach ($lines as $line) {
     $size = filesize($line);
+    $p = pathinfo($line);
+    $zipFile = $p['dirname'] . '/' . $p['filename'] . '.zip';
     if ($size > $skip) {
-        continue; //github don't like file larger than 100M
+        if (!file_exists($zipFile)) {
+            $zip->open($zipFile,  ZipArchive::CREATE);
+            $zip->addFile($line);
+            $zip->close();
+        }
     }
-    $currentSize += $size;
-    exec("/usr/bin/git add '{$line}'");
+    if (!file_exists($zipFile)) {
+        $currentSize += $size;
+        exec("/usr/bin/git add '{$line}'");
+    } else {
+        $size = filesize($zipFile);
+        if ($size > $skip) {
+            continue;
+        } else {
+            $currentSize += $size;
+            exec("/usr/bin/git add '{$zipFile}'");
+        }
+    }
+    continue;
+
     if ($currentSize > $limit) {
         $now = date('Y-m-d H:i:s');
         exec("cd {$path} && /usr/bin/git commit --author 'auto commit <noreply@localhost>' -m 'auto update @ {$now}'");
